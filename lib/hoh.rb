63500a01289c4e4acad3092b72564b136b689ff4
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-01 14:27:38 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -40,13 +40,13 @@ def naming_system
       sub_category:{
      label: "Order",
 
-                sub_category: { 
-            label: "Family",
-                  sub_category: {
-                    label: "Genus",
-                    sub_category: {
-                      label: "Species",
-                      sub_category: nil } } } } } } }
+      sub_category: { 
+      label: "Family",
+       sub_category: {
+        label: "Genus",
+        sub_category: {
+        label: "Species",
+        sub_category: nil } } } } } } }
   }
   # Remember:
   #  Kingdom
